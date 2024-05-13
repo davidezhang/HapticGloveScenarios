e39074b9d724a278e2130eef632050200ced6d3d
Davide Zhang <11183058+davidezhang@users.noreply.github.com>
2024-05-12 23:15:07 -0700

    Basic collision force detection using collision velocity

--- Assets/RoboFluid/HapticIllusions/Scripts/ColliderController.cs
+++ Assets/RoboFluid/HapticIllusions/Scripts/ColliderController.cs
@@ -8,6 +8,8 @@ public class ColliderController : MonoBehaviour
 {
     public TextMeshProUGUI debugText;
     private string originalLabel;
+    private Vector3 collisionForce = Vector3.zero;
+    private float force;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +26,36 @@ public class ColliderController : MonoBehaviour
     // When a collision with a model part collider is detected, send an event to the HapticRouter
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("ColliderController: Collision with model part collider");
-        debugText.text = "Collided!";
+        //Debug.Log("ColliderController: Collision with model part collider");
+
+        //collisionForce = collision.impulse / Time.fixedDeltaTime;
+       if (collision.collider.tag != "ignoreCollision")
+        {
+            Vector3 collisionVelocity = collision.relativeVelocity;
+            force = collisionVelocity.magnitude * 100f;
+            debugText.text = "Collided!" + (int)force;
+        }
+       
+        
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag != "ignoreCollision")
+        {
+            debugText.text = "Collided!" + (int)force;
+        }
+            
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        debugText.text = originalLabel;
+        if (collision.collider.tag != "ignoreCollision")
+        {
+            debugText.text = originalLabel;
+        }
+        
     }
 
 
